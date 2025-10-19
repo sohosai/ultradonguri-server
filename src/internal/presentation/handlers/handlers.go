fe3a5750722624da823fbf9e16e1b9cd1f0f7dd5
tomi-saku <72648609+tomi-saku@users.noreply.github.com>
2025-10-19 14:41:32 +0900

    各エンドポイントのルーティングを設定

--- src/internal/presentation/handlers/handlers.go
+++ src/internal/presentation/handlers/handlers.go
@@ -1,16 +1,9 @@
 package handlers
 
 import (
-	"log"
-	"log/slog"
-	"net/http"
-
 	"github.com/sohosai/ultradonguri-server/internal/domain/repositories"
-	"github.com/sohosai/ultradonguri-server/internal/presentation/model/requests"
-	"github.com/sohosai/ultradonguri-server/internal/presentation/model/responses"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sohosai/ultradonguri-server/internal/infrastructure/file"
 	"github.com/sohosai/ultradonguri-server/internal/infrastructure/telop/websocket"
 )
 
@@ -29,192 +22,48 @@ func NewHandler(audio repositories.AudioService, telop repositories.TelopStore, 
 }
 
 func (h *Handler) Handle(r *gin.Engine) {
-	r.GET("/health", func(c *gin.Context) {
-		message := "Hello World"
-		c.IndentedJSON(http.StatusOK, message)
-	})
 
-	// /force_mute
-	r.POST("/force_mute", func(c *gin.Context) {
-		var muteReq requests.MuteStateRequest //jsonを受け取るため
-		if err := c.ShouldBindJSON(&muteReq); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	healthHandler := HealthHandler{}
 
-		newMuteState := muteReq.ToDomainMute() //domainの型に変換
+	muteHandler := MuteHandler{
+		AudioService: h.AudioService,
+	}
 
-		if err := h.AudioService.SetForceMute(newMuteState.IsMuted); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+	performancesHandler := PerformancesHandler{}
 
-		c.JSON(http.StatusOK, gin.H{"ok": true})
-	})
+	performanceHandler := PerformanceHandler{
+		AudioService: h.AudioService,
+		TelopStore:   h.TelopStore,
+	}
 
-	//仕様からなくなっている
-	// r.GET("/force_mute", func(c *gin.Context) {
-	// 	state, err := h.AudioService.GetMute()
-	// 	if err != nil {
-	// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-	// 		return
-	// 	}
+	conversionHandlers := ConversionHandlers{
+		AudioService: h.AudioService,
+		TelopStore:   h.TelopStore,
+		wsService:    h.wsService,
+	}
 
-	// 	newMuteState := responses.NewMuteStateResponse(state) //返すjsonに変換するための型変換
+	websocketHandlers := WebsocketHandlers{
+		AudioService: h.AudioService,
+		TelopStore:   h.TelopStore,
+		wsService:    h.wsService,
+	}
 
-	// 	c.JSON(http.StatusOK, newMuteState)
-	// })
+	r.GET("/health", healthHandler.GetHealth)
 
-	// /performances
-	r.GET("/performances", func(c *gin.Context) {
-		perfs, err := file.GetPerformances()
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-			return
-		}
+	r.POST("/force_mute", muteHandler.PostForceMute)
+	r.GET("/performances", performancesHandler.GetPerformances)
 
-		newPerfState := responses.NewPerformancesResponse(perfs)
+	performanceRoutes := r.Group("/performance")
+	{
+		performanceRoutes.POST("/start", performanceHandler.PostPerformanceStart)
+		performanceRoutes.POST("/music", performanceHandler.PostPerformanceMusic)
+	}
 
-		c.JSON(http.StatusOK, newPerfState)
-	})
+	conversionRoutes := r.Group("/conversion")
+	{
+		conversionRoutes.POST("/start", conversionHandlers.PostConversionStart)
+		conversionRoutes.POST("/cm-mode", conversionHandlers.PostConversionCMMode)
+	}
 
-	// /performance
-	r.POST("/performance/start", func(c *gin.Context) {
-		var perf requests.PerformanceRequest
-		if err := c.ShouldBindJSON(&perf); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		//テロップは後で 受け取る型が変わっているため要変更
-		// perfEntity := perf.ToDomainPerformance()
-
-		// h.TelopStore.SetPerformanceTelop(perfEntity)
-		// telopMessage := h.TelopStore.GetCurrentTelopMessage()
-		// if telopMessage.IsSome() {
-		// 	h.wsService.PushTelop(telopMessage.Unwrap())
-		// }
-
-		h.AudioService.SetIsConversion(false)
-		c.JSON(http.StatusOK, gin.H{"ok": true})
-	})
-
-	r.POST("/performance/music", func(c *gin.Context) {
-		var perf requests.MusicPostRequest
-		if err := c.ShouldBindJSON(&perf); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		musicEntity := perf.ToDomainMusicPost()
-
-		//テロップは後で　受け取る型が変わっているため要変更
-		// h.TelopStore.SetPerformanceTelop(perfEntity)
-		// telopMessage := h.TelopStore.GetCurrentTelopMessage()
-		// if telopMessage.IsSome() {
-		// 	h.wsService.PushTelop(telopMessage.Unwrap())
-		// }
-
-		//performance中しか/musicを呼べなくするなら、そのステートもいるかも
-		//一旦簡易的にこちらでもisConersionをfalseにしておく
-		h.AudioService.SetIsConversion(false)
-		err := h.AudioService.SetShouldBeMuted(musicEntity.Music.ShouldBeMuted)
-		if err != nil {
-			//後でエラーを細かくする
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		if musicEntity.Music.ShouldBeMuted {
-			h.AudioService.SetMute(true)
-		} else {
-			h.AudioService.SetMute(false)
-		}
-
-		c.JSON(http.StatusOK, gin.H{"ok": true})
-	})
-
-	// /conversion
-	r.POST("/conversion/start", func(c *gin.Context) {
-		var conv requests.ConversionRequest
-		if err := c.ShouldBindJSON(&conv); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		convEntity := conv.ToDomainConversion()
-
-		h.TelopStore.SetConversionTelop(convEntity)
-		telopMessage := h.TelopStore.GetCurrentTelopMessage()
-		if telopMessage.IsSome() {
-			h.wsService.PushTelop(telopMessage.Unwrap())
-		}
-
-		h.AudioService.SetIsConversion(true)
-		c.JSON(http.StatusOK, gin.H{"ok": true})
-	})
-
-	r.POST("/conversion/cm-mode", func(c *gin.Context) {
-		var conv requests.CMStateRequest
-		if err := c.ShouldBindJSON(&conv); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		convEntity := conv.ToDomainCMState()
-
-		//ここでのテロップはいらないかも
-		// h.TelopStore.SetConversionTelop(convEntity)
-		// telopMessage := h.TelopStore.GetCurrentTelopMessage()
-		// if telopMessage.IsSome() {
-		// 	h.wsService.PushTelop(telopMessage.Unwrap())
-		// }
-
-		if convEntity.IsCMMode {
-			err := h.AudioService.SetCMScene()
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-		} else {
-			err := h.AudioService.SetMute(false)
-			if err != nil {
-				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-				return
-			}
-		}
-
-		c.JSON(http.StatusOK, gin.H{"ok": true})
-	})
-
-	// /ws
-	r.GET("/ws", func(c *gin.Context) {
-		wsConnection, err := websocket.Upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			log.Println("upgrade:", err)
-			return
-		}
-
-		slog.Info("New websocket connection is established")
-		h.wsService.AddConnection(wsConnection)
-
-		defer func() {
-			h.wsService.RemoveConnection(wsConnection)
-		}()
-
-		for {
-			mt, msg, err := wsConnection.ReadMessage()
-			if err != nil {
-				log.Println("read error:", err)
-				break
-			}
-			log.Println("Received:", string(msg))
-
-			err = wsConnection.WriteMessage(mt, []byte("Hello from server!"))
-			if err != nil {
-				log.Println("write error:", err)
-				break
-			}
-		}
-	})
+	r.GET("/ws", websocketHandlers.GetWebsocketConnection)
 }
