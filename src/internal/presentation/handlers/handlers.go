39c224455ec55d0bf43a2eed05d94d22fc4c7c3a
sugi404 <199529692+sugi404@users.noreply.github.com>
2026-09-26 18:24:24 +0900

    handlersのtelop部分を削除

--- src/internal/presentation/handlers/handlers.go
+++ src/internal/presentation/handlers/handlers.go
@@ -4,19 +4,16 @@ import (
 	"github.com/sohosai/ultradonguri-server/internal/domain/repositories"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sohosai/ultradonguri-server/internal/infrastructure/telop/websocket"
 )
 
 type Handler struct {
 	SceneManager repositories.SceneManager
-	TelopManager repositories.TelopManager
 	wsService    *websocket.WebSocketHub
 }
 
-func NewHandler(scene repositories.SceneManager, telop repositories.TelopManager, wsHub *websocket.WebSocketHub) *Handler {
+func NewHandler(scene repositories.SceneManager, wsHub *websocket.WebSocketHub) *Handler {
 	return &Handler{
 		SceneManager: scene,
-		TelopManager: telop,
 		wsService:    wsHub,
 	}
 }
@@ -27,20 +24,17 @@ func (h *Handler) Handle(r *gin.Engine) {
 
 	muteHandler := MuteHandler{
 		SceneManager: h.SceneManager,
-		TelopManager: h.TelopManager,
 	}
 
 	performancesHandler := PerformancesHandler{}
 
 	performanceHandler := PerformanceHandler{
 		SceneManager: h.SceneManager,
-		TelopManager: h.TelopManager,
 		wsService:    h.wsService,
 	}
 
 	conversionHandlers := ConversionHandlers{
 		SceneManager: h.SceneManager,
-		TelopManager: h.TelopManager,
 		wsService:    h.wsService,
 	}
 
@@ -50,7 +44,6 @@ func (h *Handler) Handle(r *gin.Engine) {
 
 	websocketHandlers := WebsocketHandlers{
 		SceneManager: h.SceneManager,
-		TelopManager: h.TelopManager,
 		wsService:    h.wsService,
 	}
 
