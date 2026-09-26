473b31ce86f3309d8f8579fa6bcb4cc7a35043d3
sugi404 <199529692+sugi404@users.noreply.github.com>
2026-09-26 20:09:32 +0900

    handlersのエンドポイント調整

--- src/internal/presentation/handlers/handlers.go
+++ src/internal/presentation/handlers/handlers.go
@@ -8,13 +8,11 @@ import (
 
 type Handler struct {
 	SceneManager repositories.SceneManager
-	wsService    *websocket.WebSocketHub
 }
 
-func NewHandler(scene repositories.SceneManager, wsHub *websocket.WebSocketHub) *Handler {
+func NewHandler(scene repositories.SceneManager) *Handler {
 	return &Handler{
 		SceneManager: scene,
-		wsService:    wsHub,
 	}
 }
 
@@ -28,23 +26,8 @@ func (h *Handler) Handle(r *gin.Engine) {
 
 	performancesHandler := PerformancesHandler{}
 
-	performanceHandler := PerformanceHandler{
-		SceneManager: h.SceneManager,
-		wsService:    h.wsService,
-	}
-
 	conversionHandlers := ConversionHandlers{
 		SceneManager: h.SceneManager,
-		wsService:    h.wsService,
-	}
-
-	copyrightHandler := CopyRightHandler{
-		wsService: h.wsService,
-	}
-
-	websocketHandlers := WebsocketHandlers{
-		SceneManager: h.SceneManager,
-		wsService:    h.wsService,
 	}
 
 	r.GET("/health", healthHandler.GetHealth)
@@ -52,19 +35,9 @@ func (h *Handler) Handle(r *gin.Engine) {
 	r.POST("/force_mute", muteHandler.PostForceMuted)
 	r.GET("/performances", performancesHandler.GetPerformances)
 
-	performanceRoutes := r.Group("/performance")
-	{
-		performanceRoutes.POST("/start", performanceHandler.PostPerformanceStart)
-		performanceRoutes.POST("/music", performanceHandler.PostPerformanceMusic)
-	}
-
 	conversionRoutes := r.Group("/conversion")
 	{
-		conversionRoutes.POST("/start", conversionHandlers.PostConversionStart)
 		conversionRoutes.POST("/cm-mode", conversionHandlers.PostConversionCMMode)
 	}
 
-	r.POST("/display-copyright", copyrightHandler.PostDisplayCopyRight)
-
-	r.GET("/ws", websocketHandlers.GetWebsocketConnection)
 }
